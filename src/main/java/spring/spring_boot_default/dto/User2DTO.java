b6972eecea8abe95413cf3f672a27bb4e2d8b132
simuneu <worksimu@gmail.com>
2025-08-29 18:40:23 +0900

    dto, vo controller 예제

---
+++ src/main/java/spring/spring_boot_default/dto/User2DTO.java
@@ -0,0 +1,13 @@
+package spring.spring_boot_default.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User2DTO {
+    private String id;
+    private String password;
+}
+
+
