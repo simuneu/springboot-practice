9230bcbaa6b54b0d16de6af3601ada59c7d01491
simuneu <worksimu@gmail.com>
2025-09-01 17:52:23 +0900

    repository

--- spring-boot-default/src/main/java/spring/spring_boot_default/vo/UserVO.java
+++
@@ -1,9 +0,0 @@
-package spring.spring_boot_default.vo;
-
-import lombok.Getter;
-
-@Getter
-public class UserVO {
-    private String name;
-    private int age;
-}
--- spring-boot-default/src/main/resources/static/hi.js
+++
@@ -1,5 +0,0 @@
-const img = document.getElementById("js_img");
-
-img.addEventListener("click", ()=>{
-    alert("이미지 클릭 !")
-});
