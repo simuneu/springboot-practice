b98668328879f96e21b6d5487d9e19f61d3d49f4
simuneu <worksimu@gmail.com>
2025-09-01 13:15:38 +0900

    Add spring-boot-mybatis project

---
+++ spring-boot-mybatis/src/main/java/spring/spring_boot_mybtis/SpringBootMybtisApplication.java
@@ -0,0 +1,13 @@
+package spring.spring_boot_mybtis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybtisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybtisApplication.class, args);
+	}
+
+}
